78303d32c2081726c45683bf08138035f3f42d9c
PhamNguyenHungRG <163432651+PhamNguyenHungRG@users.noreply.github.com>
2024-03-28 13:40:13 +0700

    hưng test

--- WEBSAIGONGLISTEN/Controllers/HomeController.cs
+++ WEBSAIGONGLISTEN/Controllers/HomeController.cs
@@ -48,7 +48,7 @@ namespace WEBSAIGONGLISTEN.Controllers
 
             var blogs = await _context.Blogs
                                       .Where(b => b.PlaceName.Contains(query)) // 'PlaceName' là tên cột chứa tên địa danh
-                                      .ToListAsync();hhhhh
+                                      .ToListAsync();ooo
             // Sau đó, trả về view kết quả tìm kiếm
             return View();
         }
